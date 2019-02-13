cf07de608d697d153bd27eb8b95fb777c78fa439
Benjamin Mpolokoso <ben.mpolokoso@gmail.com>
2019-02-13 07:17:27 +0200

    Fix: Edit Records

--- public/index.php
+++ public/index.php
@@ -5,4 +5,38 @@
  * Date: 07/02/2019
  * Time: 9:06 PM
  */
-echo "Hello World Branch"
+
+main::start();
+
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $table = html::generateTable($records);
+        $table = 'test';
+        system::printPage($table);
+    }
+}
+
+class csv
+{
+    static public function getRecords()  {
+
+        $records= 'test records';
+     return $records;
+    }
+}
+class html{
+    static public function generateTable($records) {
+
+        $table = $records;
+
+    return $table;
+    }
+}
+class system {
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}
