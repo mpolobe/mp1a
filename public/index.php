fb58718e58bb6e2aba82bebbe86481968db51fcb
Benjamin Mpolokoso <ben.mpolokoso@gmail.com>
2019-02-14 20:24:53 +0200

    Fix: Declared Variable $records

--- public/index.php
+++ public/index.php
@@ -1,42 +1,145 @@
- <?php
 /**
- * Created by PhpStorm.
- * User: benjamin.mpolokoso
- * Date: 07/02/2019
- * Time: 9:06 PM
- */
+* Created by PhpStorm.
+* User: benjamin.mpolokoso
+* Date: 07/02/2019
+* Time: 9:06 PM
+*/
 
-main::start();
 
+<?php
+main::start("TechCrunchcontinentalUSA.csv");
 class main {
-
-    static public function start() {
-        $records = csv::getRecords();
-        $table = html::generateTable($records);
-        $table = 'test';
-        system::printPage($table);
+    static public function start($filename) {
+        $records = csvFileReader::getRecords($filename);
+        staticHtml::bootstrapTemplate($records);
     }
 }
-
-class csv
+class printer {
+    public static function echoString($string) {
+        echo $string;
+    }
+}
+class recordsGenerator {
+    public static function generateRecordArray($records) {
+        $recordArray = array();
+        foreach ($records as $record) {
+            array_push($recordArray, $record);
+        }
+        return $recordArray;
+    }
+}
+class staticHtml
 {
-    static public function getRecords()  {
-
-        $records= 'test records';
-     return $records;
+    public static function trTagGeneratorStart()
+    {
+        printer::echoString( "<tr>");
+    }
+    public static function trTagGeneratorEnd()
+    {
+        printer::echoString( "</tr>");
+    }
+    public static function tableTagGenerator($type, $item)
+    {
+        printer::echoString( "<" . $type . ">" . $item . "</" . $type . " >");
+    }
+    public static function bootstrapLinkGenerator() {
+        printer::echoString( "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css\">");
+    }
+    public static function htmlTagGeneratorStart(){
+        printer::echoString( "<html>");
+    }
+    public static function htmlTagGeneratorEnd(){
+        printer::echoString( "</html>");
+    }
+    public static function bodyTagGeneratorStart(){
+        printer::echoString( "<body>");
+    }
+    public static function bodyTagGeneratorEnd(){
+        printer::echoString( "</body>");
+    }
+    public static function tableTagGeneratorStart()
+    {
+        printer::echoString( "<table class='table table-striped'>\n\n");
+    }
+    public static function tableTagGeneratorEnd()
+    {
+        printer::echoString( "</table>");
+    }
+    public static function bootstrapTemplate($record) {
+        self::htmlTagGeneratorStart();
+        self::bootstrapLinkGenerator();
+        self::bodyTagGeneratorStart();
+        self::tableTagGeneratorStart();
+        dynamicHtmlGenerator::rowGenerator(recordsGenerator::generateRecordArray($record));
+        self::tableTagGeneratorEnd();
+        self::bodyTagGeneratorEnd();
+        self::htmlTagGeneratorEnd();
     }
 }
-class html{
-    static public function generateTable($records) {
-
-        $table = $records;
-
-    return $table;
+class dynamicHtmlGenerator {
+    public static function rowGenerator($array) {
+        $count = 0;
+        foreach ($array as $item) {
+            //I dont use tableTagGenerator here because it echoes the same color rows if I use it
+            staticHtml::trTagGeneratorStart();
+            self::cellGenerator($item, $count);
+            staticHtml::trTagGeneratorEnd();
+            $count++;
+        }
+    }
+    public static function cellGenerator($item, $count) {
+        foreach ($item as $i) {
+            if ($count == 0) {
+                staticHtml::tableTagGenerator("th", $i);
+            } else {
+                staticHtml::tableTagGenerator("td", $i);
+            }
+        }
+}
+}
+class csvFileReader {
+    /**
+     * @param $filename
+     * @return TYPE_NAME
+     */
+    static public function getRecords($filename) {
+        $records = 0;
+        $file = fopen($filename, "r");
+        $fieldNames = array();
+        $count = 0;
+        while (! feof($file)) {
+            $record = fgetcsv($file);
+            if ($count == 0) {
+                $fieldNames = $record;
+            } else {
+                /** @var TYPE_NAME $records */
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
     }
 }
-class system {
-    static public function printPage($page) {
-
-        echo $page;
+class record {
+    public function __construct(Array $fieldNames = null, $values = null) {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array)$this;
+        return $array;
+    }
+    public function createProperty($name, $value) {
+        $this->{$name} = $value;
     }
 }
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+}
+?>
